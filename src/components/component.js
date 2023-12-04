344a34dcb30c8cb35f52bf3dbdbaad092e21e620
godspeed-03 <satyam2001anand@gmail.com>
2023-12-04 18:17:24 +0530

    Fixed Hydration Issue in serialized promises

--- src/components/component.js
+++ src/components/component.js
@@ -1,10 +1,19 @@
 "use client"
-export function Component ({
-  delayedMessage
-}) {
+import React from 'react'
+export function Component({ delayedMessage }) {
+  const [message, setMessage] = React.useState(null);
+
+    // Resolve the promise and update the component's state
+  React.useEffect(() => {
+
+    delayedMessage.then((resolvedMessage) => {
+      setMessage(resolvedMessage);
+    });
+  }, [delayedMessage]);
+
   return (
     <div>
-      {delayedMessage}
+      {message}
     </div>
-  )
+  );
 }
